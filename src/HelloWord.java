293fa8d74a4eb4b18942e29c22f133293e25a931
Leo_Chen <Leo66_Chen@pegatroncorp.com>
2019-10-02 09:12:11 +0800

    add static method in HelloWord by 2222

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -3,4 +3,8 @@ public class HelloWord {
 	public static void helloWorld() {
 		System.out.println("Hello World!");
 	}
+	
+	public static void helloString(String s) {
+		System.out.println("Hello "+s+"!");
+	}
 }
